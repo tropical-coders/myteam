d2dbdc63153fe3728b450f0473de752e87b79b5c
pradeep3122003 <pradeepkh312@gmail.com>
2025-08-08 18:23:23 +0000

    email modules api

--- backend/routes/profile_manage.py
+++ backend/routes/profile_manage.py
@@ -4,6 +4,7 @@ from utils.validators import valid_email
 from models import Users, Profile
 from extension import db
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from send_email import email_send
 
 profile_bp=Blueprint('profile',__name__,url_prefix="/api/v1/profile")
 
@@ -22,5 +23,6 @@ def profile():
     return jsonify({"message": "something went wrong"}), 400
 
   if profile.status=="inactive":
+    email_send()
     return jsonify({"message": "varify your email to update profile"}), 400
   return jsonify({"message":"profile found"}), 200
--- backend/send_email.py
+++ backend/send_email.py
@@ -4,40 +4,41 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 load_dotenv()
-port = os.getenv("SMTP_PORT")
-smtp_server = os.getenv("SMTP_SERVER")
-sender_email = os.getenv("EMAIL")
-password = os.getenv("EMAIL_PASS")
+def email_send():
+ port = os.getenv("SMTP_PORT")
+ smtp_server = os.getenv("SMTP_SERVER")
+ sender_email = os.getenv("EMAIL")
+ password = os.getenv("EMAIL_PASS")
 
-receiver_email = "pradeepkh312@gmail.com"
+ receiver_email = "pradeepkh312@gmail.com"
 
-subject = "HTML Email without Attachment"
+ subject = "HTML Email without Attachment"
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-template_path = os.path.join(base_dir, "templates", "otp.html")
+ base_dir = os.path.dirname(os.path.abspath(__file__))
+ template_path = os.path.join(base_dir, "templates", "otp.html")
 
-try:
+ try:
     with open(template_path, 'r') as file:
         html = file.read()
     print("Successfully read the HTML template.")
 
-except FileNotFoundError:
+ except FileNotFoundError:
     print(f"Error: The file was not found at {template_path}")
     html = None
-except Exception as e:
+ except Exception as e:
     print(f"An error occurred while reading the file: {e}")
 
 
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
+ message = MIMEMultipart()
+ message["From"] = sender_email
+ message["To"] = receiver_email
+ message["Subject"] = subject
 
-message.attach(MIMEText(html, "html"))
+ message.attach(MIMEText(html, "html"))
 
-with smtplib.SMTP(smtp_server, port) as server:
+ with smtplib.SMTP(smtp_server, port) as server:
     server.starttls()
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message.as_string())
 
-print('Sent')
+ print('Sent')
