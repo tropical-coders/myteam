28786b78e2b82dbd00250981a3705bb7f33cb7f1
pradeep3122003 <pradeepkh312@gmail.com>
2025-08-29 14:16:02 +0000

    jwt cookie stored in session

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from extension import db, jwt
 load_dotenv()
 def create_app(config_class=Config):
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
     app.config.from_object(config_class)
 
     db.init_app(app)
