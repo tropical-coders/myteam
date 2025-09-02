b7eafa24610cfdb25971ab085152bd3812cdd3c2
pradeep3122003 <pradeepkh312@gmail.com>
2025-09-02 16:43:16 +0000

    jwt extended for 12 hours

--- backend/config.py
+++ backend/config.py
@@ -20,7 +20,7 @@ class Config:
 
     # --- Flask-JWT-Extended Configuration ---
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=12)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_COOKIE_SECURE = False
